6438bc635c7841da425240a4b323c89f9f171ed1
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-11-13 21:30:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let good= document.querySelector("#good");
+let cheap= document.querySelector("#cheap");
+let fast= document.querySelector("#fast");
+let inputs= document.querySelectorAll(".toggle");
+
+inputs.forEach(input => {
+	input.addEventListener("change", (e) => {
+		checkAndUpdateToggles(e);
+	})
+})
+
+function checkAndUpdateToggles(e) {
+	let count= 0
+	inputs.forEach(input => {
+		if(input.checked) {
+			count++;
+		}
+	})
+	if(count>2) {
+		for(let input of inputs) {
+			if(input.checked && input!==e.target) {
+				input.checked= false;
+				break;
+			}
+		}
+	}
+}
+
+
+
+
